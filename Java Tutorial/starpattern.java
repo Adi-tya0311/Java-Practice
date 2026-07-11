6227464e5af29fe4d80c35b3de109e2f6dd7d50f
Adi-tya0311 <adityajagtap1979@gmail.com>
2026-07-11 11:50:31 +0530

    Updated star pattern- Learning different patterns

--- Java Tutorial/starpattern.java
+++ Java Tutorial/starpattern.java
@@ -6,5 +6,22 @@ public class starpattern{
             }
         System.out.println("");
         }
+
+        System.out.println("Next pattern: ");
+        for(int i=5;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+        System.out.println("");
+        }
+        System.out.println("Next pattern: ");
+        for(int i=1;i<=10;i++){
+            if(i%2==0){
+                for(int j=1;j<=i;j++){
+                    System.out.print("* ");
+                }
+            }
+        System.out.println("");
+        }
     }
 }
