9cd4b39432c1cecc62cd370bf590319f2f8112e6
Adi-tya0311 <adityajagtap1979@gmail.com>
2026-07-27 22:12:51 +0530

    Basic multiplication using loop and no *

---
+++ Java Tutorial/Loopmultiply.java
@@ -0,0 +1,14 @@
+
+public class Loopmultiply{
+    public static void main(String[] args){
+        int a = 5;
+        int b = 6;
+        int sum = 0;
+
+        for(int i=1; i<=b;i++){
+            sum = sum + a;
+        }
+        System.out.println(sum); //multiplication using loop
+    }
+}
+
