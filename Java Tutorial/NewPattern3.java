ce444fd6b5334e5f6bc48b4f0b0a5f2da3bc0e51
Adi-tya0311 <adityajagtap1979@gmail.com>
2026-08-13 11:08:50 +0530

    Trying out new patterns

---
+++ Java Tutorial/NewPattern3.java
@@ -0,0 +1,125 @@
+public class NewPattern3 {
+    public static void main(String[] args){
+
+        // for(char i='A';i<=69;i++){      //My method
+        //     for(char j='A';j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println("");
+        // }
+
+        //Trial and error
+        // for(int i=65;i<=69;i++){      //My New method
+        //     for(char j='A';j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println("");
+        // }
+
+        //Trial and error
+        // for(int i=65;i<=69;i++){      //My New method
+        //     for(char j='A';j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println("");
+        // }
+
+        // System.out.println("🚀🚀");
+        //         chcp 65001
+        // $OutputEncoding = [System.Text.UTF8Encoding]::new()
+        // java Aug8.java
+
+        //Run this command in terminal to print the emojis.
+
+
+        // for(int i=1;i<=5;i++){
+        //     for(char c='A';c<='E';c++){
+        //         System.out.println(c);
+        //     }
+        // }
+                                            //Both are teachers code
+
+        // for(int i='A';i<='E';i++){  //Here we can give char input to integer var because it reads ASCII value of char.
+        //     for(char c='A';c<='E';c++){
+        //         System.out.println(c);
+        //     }
+        // }
+
+
+        //Patter 25 opposite
+        // for(char i='E';i>='A';i--){
+        //     for(char j='A';j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        //Pattern 26
+        // for(char i='A';i<='E';i++){
+        //     for(char j='A';j<=i;j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        // }
+
+        //Pattern 27
+        // for(char i='A';i<='E';i++){
+        //     for(char j='A';j<=i;j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        // }
+        // for(char i='D';i>='A';i--){
+        //     for(char j='A';j<=i;j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        // }
+
+        //Pattern 28
+        // for(char i='A';i<='E';i++){
+        //     for(char j='A';j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+        // for(char i='D';i>='A';i--){
+        //     for(char j='A';j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+
+        //Pattern 29
+        // for(int i='E';i>='A';i--){
+        //     for(char j='A';j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println("");
+        // }
+        // for(int i='B';i<='F';i++){
+        //     for(char j='A';j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println("");
+        // }
+
+        //Pattern 30
+        for(char i='F';i>='A';i--){
+            for(char j=i;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+
+        // for(int i=1;i<=5;i++){
+        //     for(int j=5;j>=i;j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
+        // }
+
+
+    }
+}
