73fb6944c2e94e4ee2057ef3449035186a15b9c7
Adi-tya0311 <adityajagtap1979@gmail.com>
2026-09-09 23:32:01 +0530

    Arrays printing

---
+++ Java Tutorial/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static void GCD(String[] args) {
+        int[] nums ={2,5,6,9,10};
+        for(int i=nums.length;i>=0;i--){
+            System.out.println(nums[i]);
+        }
+    }
+}
