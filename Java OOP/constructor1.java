bef9f7aef94ce1b5904016e022210c9f2376a43f
Adi-tya0311 <adityajagtap1979@gmail.com>
2026-09-20 22:41:53 +0530

    Basic ems using constructor

---
+++ Java OOP/constructor1.java
@@ -0,0 +1,44 @@
+// class userpass{
+//     //our container
+//     String pass;
+//     String id;
+//     userpass(String pass,String id){            //took data from object container
+//         //assign from object to our container(variable) using this
+//         this.pass = pass;
+//         this.id = id;
+//     }
+//     userpass(){     //print automatic using constructor
+//         this("Aditya","2004");      //call that constructor who has data
+//         System.out.println(pass+" "+id);     //after getting data it will print smoothly
+//     }
+// }
+// public class constructor1 {
+//     public static void main(String[] args){
+//         userpass u = new userpass();
+//         //user will only call the default constructor always
+//     }
+// }
+
+
+//Employee Management system
+
+// class Employee{
+//     String ename;String eaddr;int esalary;int eExp;
+//     Employee(String ename,String eaddr,int esalary,int eExp){
+//         this.ename = ename;
+//         this.eaddr = eaddr;
+//         this.esalary = esalary;
+//         this.eExp = eExp;
+//     }
+//     Employee(){
+//         this("E1","Pune",10000,1);
+//         System.out.println(ename+" "+eaddr+" "+esalary+" "+eExp);
+//     }
+// }
+// public class constructor1 {
+//     public static void main(String[] args){
+//         // Employee emp = new Employee();
+//         System.out.println('a'+'b');
+//         //Is this getting recorded??
+//     }
+// }
