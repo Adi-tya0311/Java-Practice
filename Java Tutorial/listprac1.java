8139d3d916aba878ea09d1f0fd94448771b30511
Adi-tya0311 <adityajagtap1979@gmail.com>
2026-09-24 08:51:12 +0530

    Practicing List in java

---
+++ Java Tutorial/listprac1.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class listprac1{
+    public static void main(String[] args) {
+        //Java Collection Start
+        List<Object> data = new ArrayList<Object>();
+        System.out.println(data);
+        data.add(1);
+        data.add("Prithvi");
+        data.add(1);
+        data.add(null);
+        data.add(23.04f);
+        System.out.println(data);
+        //System.out.println(data[0]); // this works in normal array but doesnt work here
+        System.out.println(data.get(2));
+
+        //2nd method to used list
+        ArrayList<String> l1 = new ArrayList<>();
+        l1.add("Gir gardhan ghat");l1.add("Sattu Supari");l1.add("Sattu Supari2");
+        l1.add("Gopal");
+        System.out.println(l1);
+        l1.remove(1); //we can remove elements by index or by name both
+        l1.remove("Sattu Supari2");
+        System.out.println(l1);
+
+        //Remove all - this is global method for list
+        l1.removeAll(l1);
+        System.out.println("Remove All method: "+l1);
+        l1.add("Gir gardhan ghat");l1.add("Sattu Supari");l1.add("Sattu Supari2");
+        l1.clear(); // this method is specifically for arraylist
+        System.out.println("Clear method: "+l1);
+    }
+}
